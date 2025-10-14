881a1a997ceb5a9365e12a17c2bfae1e4b0f94ab
IMIXER585 <andre5856545@gmail.com>
2025-10-14 15:29:08 -0500

    Primer commit

---
+++ src/test/java/com/UTP/TpIntegrado/TpIntegradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UTP.TpIntegrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntegradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
